d9e9a82c9588429b0c02da1b3ed354d0c7515fa5
GBFerro <giovani.ferro@usp.br>
2023-03-05 18:36:38 -0300

    Add project files.

---
+++ ParImpar/Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int n1;
+
+        Console.WriteLine("Digite um número: ");
+        n1 = int.Parse(Console.ReadLine());
+
+        if (n1 %2 == 0) {
+            Console.WriteLine("O número digitado é PAR");
+        } else {
+            Console.WriteLine("O número é IMPAR");
+                }
+    }
+}
